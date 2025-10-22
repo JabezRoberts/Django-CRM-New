c6d802e5bda97133d0801bb5213850c11908c065
Jabez Roberts <jabez.roberts137@gmail.com>
2025-10-21 20:14:03 -0500

    leads views and templates

--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
 
 # function based views
 def home_page(request):
-    return HttpResponse("Hello world!")
+    # return HttpResponse("Hello world!")
+    # return render(request, 'leads/home_page.html')
+    leads = Lead.objects.all() # returns a list of lead objects
+    context = {
+        "leads": leads
+    }
+    return render(request, 'second_page.html', context)
+# context is passing information into Django templates
+
